3440c270db81725ffacc18f1bbf0e5088f3c6395
amran2k16 <amran4hmed@gmail.com>
2019-10-06 16:01:48 +0100

    creating a quiz class component

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,54 +1,78 @@
 import React, { Component, useState } from "react";
 
-const Options = props => {
-  const [active, setactive] = useState("0");
-  console.log(active);
+const Option = props => {
+  return (
+    <button
+      onClick={e => this.setState({ selected: e.target.value })}
+      type="button"
+      disabled={this.state.submitted}
+      value={this.props.number}
+      className={`col-12 btn mb-2 ${
+        this.state.selected == this.props.number
+          ? "btn-primary"
+          : "btn-secondary"
+      }`}
+    >
+      {this.props.text}
+    </button>
+  );
+};
 
-  const buttonClicked = e => {
-    setactive(e.target.value);
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log(this.props);
+
+    this.state = {
+      selected: "0",
+      submitted: false,
+      score: 0
+    };
+  }
+
+  onSubmit = e => {
+    if (
+      this.state.selected == "1" ||
+      this.state.selected == "2" ||
+      this.state.selected == "3" ||
+      this.state.selected == "4"
+    ) {
+      setSubmitted(true);
+      if (this.state.selected == correct) {
+        console.log("correct answer was selected");
+        // Make that selected component green
+      } else {
+        console.log("That was the wrong answer");
+        // Make that selected component red
+      }
+    } else {
+      console.log("You need to select an answer");
+    }
   };
 
-  const submit = e => {
-    // if state is one of 1,2,3 or 4... then allow this otherwise send "you need to pick one!"
-    console.log("submitted");
-  };
-
-  const Button = props => {
+  render() {
     return (
-      <button
-        onClick={buttonClicked}
-        type="button"
-        value={props.number}
-        className={`col-12 btn mb-2 ${
-          active == props.number ? "btn-primary" : "btn-secondary"
-        }`}
-      >
-        {props.text}
-      </button>
-    );
-  };
-
-  return (
-    <div className="row p-1">
-      <Button number="1" text="First option" />
-      <Button number="2" text="Second option" />
-      <Button number="3" text="Third option" />
-      <Button number="4" text="Fourth option" />
-      <button onClick={submit}>Submit</button>
-    </div>
-  );
-};
-
-const Quiz = props => {
-  return (
-    <div className="row">
-      <div className="col-12">
-        <h4>What is a print statement?</h4>
-        <Options />
+      <div className="row">
+        <div className="col-12">
+          <h4>{this.props.question}</h4>
+          <div className="row p-1">
+            <Option number="1" text={this.props.option1} />
+            <Option number="2" text={this.props.option2} />
+            <Option number="3" text={this.props.option3} />
+            <Option number="4" text={this.props.option4} />
+            <button disabled={this.state.submitted} onClick={this.onSubmit}>
+              Submit
+            </button>
+            {this.state.submitted ? (
+              <button className="float-right">Next</button>
+            ) : null}
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 // Get initial props not working
 // Quizzes.getInitialProps = async function(context) {
